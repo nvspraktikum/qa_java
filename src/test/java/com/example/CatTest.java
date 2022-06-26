746ef7dfe66afde9acac4cc3f8905ff334e8a15b
vniyazov <vniyazov@aquivalabs.com>
2022-06-26 21:38:23 +0300

    updated CatTest

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -1,18 +1,22 @@
 package com.example;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
 import static org.junit.Assert.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CatTest {
 
+    @Mock
+    Feline feline;
     @Test
     public void getSound() {
-        Feline feline = Mockito.mock(Feline.class);
         Cat cat = new Cat(feline);
         String actual = cat.getSound();
         String expected = "Мяу";
@@ -21,7 +25,6 @@ public class CatTest {
 
     @Test
     public void getFood() throws Exception{
-        Feline feline = Mockito.mock(Feline.class);
         Cat cat = new Cat(feline);
         List<String> expected = List.of("Животные","Птицы", "Рыба");
         Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные","Птицы", "Рыба"));
